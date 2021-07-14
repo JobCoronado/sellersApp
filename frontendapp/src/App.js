e207ec8323a1aa48901d06fc686a19aeff90fa6c
RicardoArquimedes <richardcqt28@gmail.com>
2021-07-14 16:24:01 -0500

    updating index and app

--- frontendapp/src/App.js
+++ frontendapp/src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Intro from './components/Intro/intro';
 import Header from './components/Header/Header';
+import Home from './components/Home/Home'
 import AddOrders from './components/AddOrders/addOrders';
-
+import ShowOrders from './components/showOrders/showOrders';
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <AddOrders/>
+      <Router>
+        <Header/>
+        <Switch>
+          <Route exact path="/">
+            <Intro/>
+          </Route>
+          <Route path="/test">
+            <Home/>
+          </Route>
+          <Route path="/orders">
+            <AddOrders/>
+          </Route>
+          <Route path="/showorders">
+            <ShowOrders/>
+          </Route>
+
+        </Switch>
+      </Router>
+     
     </div>
   );
 }
