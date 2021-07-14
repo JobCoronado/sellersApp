187a272c79f183f0380806fc90a111e1573e36fd
RicardoArquimedes <richardcqt28@gmail.com>
2021-07-14 16:20:56 -0500

    adding file styles based on styled components

---
+++ frontendapp/src/components/AddOrders/addOrders.styles.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const Formlayer = styled.div`
+margin: 90px 8px 1px 8px;
+color:#201b5b;
+position: relative;
+min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+  
+
+  
+`
+export const Container = styled.div`
+
+margin-top: -5px;
+background: rgb(211,205,249);
+color:#201b5b;
+background: linear-gradient(90deg, rgba(211,205,249,1) 0%, rgba(201,238,247,1) 45%);
+  border-radius: 4px;
+
+`
+export const Div = styled.div`
+  
+  margin: 8px 8px 8px 8px;
+
+  .button-primary{
+    color: white;
+    margin-left: -8px;
+    top: 5px;
+    border-radius: 4px;
+    background: rgb(2,0,36);
+background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(32,27,91,1) 45%, rgba(19,61,69,1) 100%);
+   
+  }
+`
+
+
