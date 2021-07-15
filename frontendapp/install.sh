ccb28c7c28cab1f56794d5ad13d54890ba7a9274
RicardoArquimedes <richardcqt28@gmail.com>
2021-07-15 12:50:59 -0500

    updating install script

---
+++ frontendapp/install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+#clone repo
+git clone https://github.com/RicardoArquimedes/sellersApp.git
+# open folder and install packages and dependecies
+cd sellersApp/frontendapp/ && npm install
