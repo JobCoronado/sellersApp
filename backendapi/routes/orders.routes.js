7d115b75f3cb696078409291fe6d8dbc0dfb0391
RicardoArquimedes <richardcqt28@gmail.com>
2021-07-18 15:43:46 -0500

    updating addOrders post funcion & request

--- backendapi/routes/orders.routes.js
+++ backendapi/routes/orders.routes.js
@@ -33,7 +33,7 @@ router.get('/:id', m.mustBeInteger, async (req, res) => {
 
 /* Insert a new post */
 router.post('/', m.checkFieldsPost, async (req, res) => {
-    await post.insertPost(req.body)
+    await post.addOrders(req.body)
     .then(post => res.status(201).json({
         message: `The post #${post.OrderId} has been created`,
         content: post
